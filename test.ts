2392d7767e6d5e32931374224b675be80307399f
Shun Ueda <hasundue@gmail.com>
2022-09-23 18:24:57 +0900

    Update the test

--- test.ts
+++ test.ts
@@ -1,6 +1,14 @@
-import { assert } from "https://deno.land/std@0.157.0/testing/asserts.ts";
-import { build, setup } from "./mod.ts";
+import { assertStringIncludes } from "https://deno.land/std@0.157.0/testing/asserts.ts";
+import { build } from "./mod.ts";
+
+const decoder = new TextDecoder();
 
 Deno.test("build examples/hono/index.ts", async () => {
   await build("./examples/hono/index.ts");
+  const output = await Deno.readFile("./examples/hono/.cache/brawler/index.ts");
+
+  assertStringIncludes(
+    decoder.decode(output),
+    'import { Hono } from "./deps/deno.land/x/hono@v2.1.4/mod.js";',
+  );
 });
