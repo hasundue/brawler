7637cf83873886344802b9c85758185c4604a04c
Shun Ueda <hasundue@gmail.com>
2022-09-23 11:48:05 +0900

    Minor change

--- test.ts
+++ test.ts
@@ -2,7 +2,7 @@ import * as log from "https://deno.land/std@0.157.0/log/mod.ts";
 import { assert } from "https://deno.land/std@0.157.0/testing/asserts.ts";
 import { build, setup } from "./mod.ts";
 
-Deno.test("set log level as WARNING", () => {
+Deno.test("set the log level as WARNING", () => {
   setup({ log: "WARNING" });
 
   log.info(
