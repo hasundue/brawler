220d50604f8b8e957da24a3d0a743c164fad36a9
denopendabot <denopendabot@github.com>
2023-03-15 00:15:41 +0000

    build(deps): bump hono from 3.0.3 to 3.0.5

--- examples/hono/index.ts
+++ examples/hono/index.ts
@@ -1,4 +1,4 @@
-import { Hono } from "npm:hono@3.0.3";
+import { Hono } from "npm:hono@3.0.5";
 
 const app = new Hono();
 
--- test/hono/index.ts
+++ test/hono/index.ts
@@ -1,4 +1,4 @@
-import { Hono } from "https://deno.land/x/hono@v3.0.2/mod.ts";
+import { Hono } from "https://deno.land/x/hono@v3.0.5/mod.ts";
 
 const app = new Hono();
 
