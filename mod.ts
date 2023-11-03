bf2582a2e826844599ceb27fa05d7a555eaec09f
denopendabot <denopendabot@github.com>
2023-11-03 00:14:26 +0000

    build(deps): bump std from 0.204.0 to 0.205.0

--- mod.ts
+++ mod.ts
@@ -1,6 +1,6 @@
-import * as log from "https://deno.land/std@0.204.0/log/mod.ts";
-import { dirname, join } from "https://deno.land/std@0.204.0/path/mod.ts";
-import { signal } from "https://deno.land/std@0.204.0/signal/mod.ts";
+import * as log from "https://deno.land/std@0.205.0/log/mod.ts";
+import { dirname, join } from "https://deno.land/std@0.205.0/path/mod.ts";
+import { signal } from "https://deno.land/std@0.205.0/signal/mod.ts";
 import * as esbuild from "https://deno.land/x/esbuild@v0.19.5/mod.js";
 import { denoPlugins } from "https://deno.land/x/esbuild_deno_loader@0.8.2/mod.ts";
 import { Mutex } from "https://deno.land/x/async@v2.0.2/mutex.ts";
